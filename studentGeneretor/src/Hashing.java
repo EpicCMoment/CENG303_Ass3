d42c4a28b64e549016f5b773f7089c0931bc2d5a
Arif Fil <arif.fil@outlook.com>
2023-12-10 16:34:39 +0300

    make methods of Hashing class 'static'

--- studentGeneretor/src/Hashing.java
+++ studentGeneretor/src/Hashing.java
@@ -1,10 +1,10 @@
 public class Hashing {
 
-    public int getFirstHash(Object o, int limit) {
+    public static int getFirstHash(Object o, int limit) {
         return ( ( (13 * o.hashCode()) + 19 ) / 1001 ) % limit;
     }
 
-    public int getSecondHash(Object o, int limit) {
+    public static int getSecondHash(Object o, int limit) {
         return ( ( (7 * o.hashCode()) + 23 ) / 1001 ) % limit;
     }
 
